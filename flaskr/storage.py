5f968e2c7038a7ad638cd8eca18609290c7f3189
sulaimonwasiu <sulaimonwasiu13@gmail.com>
2024-11-01 03:05:15 +0300

    Refactored the storage class

--- flaskr/storage.py
+++ flaskr/storage.py
@@ -6,37 +6,31 @@ import uuid
 class ArticleStorage:
     def __init__(self, directory='articles'):
         self.directory = directory
-        if not os.path.exists(directory):
-            os.makedirs(directory)
+        os.makedirs(directory, exist_ok=True)
 
-    def save_article(self, title, content, id=None):
-        # id = str(uuid.uuid4())
-        filename = f"{id}.json"
-        filepath = os.path.join(self.directory, filename)
-        current_date = datetime.datetime.now().strftime("%B %d, %Y")
-        if os.path.exists(filepath):
-            # update existing blog
-            article = {
-                'id': id,
-                'title': title,
-                'content': content,
-                'date': current_date
-            }
-            with open(filepath, 'w') as f:
-                json.dump(article, f)
-        else:
-            # add new blog
-            id = str(uuid.uuid4())
-            filename = f"{id}.json"
-            filepath = os.path.join(self.directory, filename)
-            article = {
-                'id': id,
-                'title': title,
-                'content': content,
-                'date': current_date
-            }
-            with open(filepath, 'w') as f:
-                json.dump(article, f)
+    def _get_file_path(self, article_id):
+        return os.path.join(self.directory, f"{article_id}.json")
+
+    def _get_current_date(self):
+        return datetime.datetime.now().strftime("%B %d, %Y")
+
+    def _create_article_dict(self, title, content, article_id):
+        return {
+            'id': article_id,
+            'title': title,
+            'content': content,
+            'date': self._get_current_date()
+        }
+
+    def save_article(self, title, content, article_id=None):
+        if article_id is None:
+            article_id = str(uuid.uuid4())
+
+        filepath = self._get_file_path(article_id)
+        article = self._create_article_dict(title, content, article_id)
+
+        with open(filepath, 'w') as f:
+            json.dump(article, f)
 
     def get_articles(self):
         articles = []
@@ -46,14 +40,14 @@ class ArticleStorage:
                     articles.append(json.load(f))
         return articles
 
-    def get_article(self, id):
-        filepath = os.path.join(self.directory, f"{id}.json")
+    def get_article(self, article_id):
+        filepath = self._get_file_path(article_id)
         if os.path.exists(filepath):
             with open(filepath) as f:
                 return json.load(f)
         return None
 
-    def delete_article(self, id):
-        filepath = os.path.join(self.directory, f"{id}.json")
+    def delete_article(self, article_id):
+        filepath = self._get_file_path(article_id)
         if os.path.exists(filepath):
             os.remove(filepath)
